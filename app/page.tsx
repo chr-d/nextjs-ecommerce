d7581713f46bedf0688ce8d57bd30ff901734d0b
chr-d <275697199+chr-d@users.noreply.github.com>
2026-09-03 14:54:41 +0200

    add product card and home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <div>Hello World</div>;
+import ProductCard from "@/components/ProductCard";
+import { getCategories, getProducts } from "@/lib/api";
+import Link from "next/link";
+
+export default async function Home() {
+  const [categories, products] = await Promise.all([
+    getCategories(),
+    getProducts(),
+  ]);
+
+  return (
+    <div className="mt-4 flex flex-col items-center">
+      <ul className="menu menu-horizontal">
+        {categories?.map((category, index) => (
+          <li className="bg-base-300 mx-2 capitalize" key={index}>
+            <Link href={`/category/${category}`}>{category}</Link>
+          </li>
+        ))}
+      </ul>
+      <div className="mt-4 flex flex-wrap justify-center gap-4">
+        {products?.map((item) => (
+          <ProductCard key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ components/ProductCard.tsx
@@ -0,0 +1,67 @@
+import { Product } from "@/lib/api";
+import { formatCurrency } from "@/lib/formatCurrency";
+import { Route } from "next";
+import Link from "next/link";
+
+export default function ProductCard({ item }: { item: Product }) {
+  // const { cart, addToCart, removeFromCart } = useCart();
+
+  // const cartItem = cart.items.find((i) => i.id === item.id);
+
+  // const handleAddToCart = () => {
+  //   addToCart({
+  //     id: item.id,
+  //     title: item.title,
+  //     price: item.price,
+  //     image: item.image,
+  //   });
+  // };
+  // const handleRemoveFromCart = () => {
+  //   removeFromCart(item.id);
+  // };
+  return (
+    <div className="card bg-base-200 w-96 shadow-sm">
+      <figure className="bg-white p-4 h-48">
+        <img
+          className="h-full"
+          src={item.image}
+          alt={item.title}
+          loading="lazy"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{item.title}</h2>
+        <p className="truncate">{item.description}</p>
+        <div className="badge badge-outline text-xs capitalize">
+          <Link href={`/category/${item.category}` as Route}>
+            {item.category}
+          </Link>
+        </div>
+        <div className="card-actions justify-end">
+          <span className="self-center text-xl">
+            {formatCurrency(item.price)}
+          </span>
+
+          {/* {!cartItem ? (
+            <button className="btn btn-primary" onClick={handleAddToCart}>
+              Add to cart
+            </button>
+          ) : (
+            <>
+              <button
+                className="btn btn-primary"
+                onClick={handleRemoveFromCart}
+              >
+                -
+              </button>
+              <span className="self-center text-xl">{cartItem.quantity}</span>
+              <button className="btn btn-primary" onClick={handleAddToCart}>
+                +
+              </button>
+            </>
+          )} */}
+        </div>
+      </div>
+    </div>
+  );
+}
