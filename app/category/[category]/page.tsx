d5b6c9a02720b2775e8e6c74ff9fca468ea88efe
chr-d <275697199+chr-d@users.noreply.github.com>
2026-09-03 15:04:46 +0200

    add category page

--- app/category/[category]/page.tsx
+++ app/category/[category]/page.tsx
@@ -1,3 +1,21 @@
-export default function Category() {
-  return <div>Category Page</div>;
+import ProductCard from "@/components/ProductCard";
+import { getProductsByCategory } from "@/lib/api";
+
+export default async function Category({
+  params,
+}: PageProps<"/category/[category]">) {
+  const { category } = await params;
+  const products = await getProductsByCategory(category);
+  return (
+    <div className="flex flex-col items-center">
+      <h2 className="my-4 text-2xl capitalize">
+        {decodeURIComponent(category)}
+      </h2>
+      <div className="flex flex-wrap justify-center gap-4">
+        {products?.map((item) => (
+          <ProductCard key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
 }
